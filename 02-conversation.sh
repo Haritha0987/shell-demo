2fe51e97e1defe731843de7ce14a2fdc2f0d1b9d
Haritha <hari.devops2025@gmail.com>
2025-05-16 13:21:39 +0530

    hello world

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Modi:: Hey Trump, How are you?"
+echo "Trump:: Hello Modi, I am fine. How are you doing."
+echo "Modi:: I am fine too. What's up?"
+echo "Trump:: Nothing, just going to Mars now, will you come?"
+echo "Modi:: Sorry, you carry on! I will come once you come back."
